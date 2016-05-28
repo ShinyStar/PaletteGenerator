16e74455bc6486ee2ccecd777fd191035ccab9fa
Silvia (Star) Barbero Rodriguez <silvi.shinystar@gmail.com>
2016-05-28 18:34:05 +0200

    Added ads, modes and fixed base generation

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -31,20 +31,44 @@ namespace PaletteGenerator {
             this.InitializeComponent();
         }
 
-        private void randomizePalette(object sender, RoutedEventArgs e) {
+        private void noBase(object sender, RoutedEventArgs e) {
             var random = new Random();
-            var basecolor = random.Next(0x100000);
+            var basecolor = random.Next(0x100000);            
+            randomizePalette(basecolor);
+        }
+
+        private void useBase(object sender, RoutedEventArgs e) {
+            var color1=colorText1.Text.Substring(1, 6);
+            var basecolor = Int32.Parse(color1, System.Globalization.NumberStyles.HexNumber);
+            randomizePalette(basecolor);
+        }
+
+        private void randomizePalette(int basecolor) {
+            var random = new Random();
             var color1 = String.Format("#{0:X6}", basecolor);
-            var hex2 = basecolor + 0x202020;
-            var color2 = String.Format("#{0:X6}", hex2);
-            var hex3 = hex2 + 0x202020;
-            var color3 = String.Format("#{0:X6}", hex3);
-            var hex4 = hex3 + 0x202020;
-            var color4 = String.Format("#{0:X6}", hex4);
-            var hex5 = hex4 + 0x202020;
-            var color5 = String.Format("#{0:X6}", hex5);
-            var hex6 = 0xFFFFFF ^ basecolor;
-            var color6 = String.Format("#{0:X6}", hex6);
+
+            string color2 = null, color3 = null, color4 = null, color5 = null, color6 = null;
+
+            if (randomMode.IsChecked.Equals(true)) {
+                var mode = random.Next(6);
+                if (mode == 1) {
+                    crazy(basecolor, ref color2, ref color3, ref color4, ref color5, ref color6);
+                } else if (mode == 2) {
+                    smooth(basecolor, ref color2, ref color3, ref color4, ref color5, ref color6);
+                } else if (mode == 3 || mode == 4) {
+                    varied(basecolor, ref color2, ref color3, ref color4, ref color5, ref color6);
+                } else {
+                    accent(basecolor, ref color2, ref color3, ref color4, ref color5, ref color6);
+                }
+            }else if (smoothMode.IsChecked.Equals(true)) {
+                smooth(basecolor, ref color2, ref color3, ref color4, ref color5, ref color6);
+            } else if (accentMode.IsChecked.Equals(true)) {
+                accent(basecolor, ref color2, ref color3, ref color4, ref color5, ref color6);
+            } else if (variedMode.IsChecked.Equals(true)) {
+                varied(basecolor, ref color2, ref color3, ref color4, ref color5, ref color6);
+            } else {
+                crazy(basecolor, ref color2, ref color3, ref color4, ref color5, ref color6);
+            }
 
             colorText1.Text = color1;
             colorText2.Text = color2;
@@ -66,6 +90,62 @@ namespace PaletteGenerator {
             colorRect4.Fill = StringToBrushNoA(color4);
             colorRect5.Fill = StringToBrushNoA(color5);
             colorRect6.Fill = StringToBrushNoA(color6);
+        }
+
+        private void accent(int basecolor, ref string color2, ref string color3, ref string color4, ref string color5, ref string color6) {
+
+            var hex2 = basecolor + 0x202020;
+            color2 = String.Format("#{0:X6}", hex2);
+            var hex3 = hex2 + 0x202020;
+            color3 = String.Format("#{0:X6}", hex3);
+            var hex4 = hex3 + 0x202020;
+            color4 = String.Format("#{0:X6}", hex4);
+            var hex5 = hex4 + 0x202020;
+            color5 = String.Format("#{0:X6}", hex5);
+            var hex6 = 0xFFFFFF ^ basecolor;
+            color6 = String.Format("#{0:X6}", hex6);
+        }
+
+        private void varied(int basecolor, ref string color2, ref string color3, ref string color4, ref string color5, ref string color6) {
+
+            var hex2 = basecolor + 0x202020;
+            color2 = String.Format("#{0:X6}", hex2);
+            var hex3 = hex2 + 0x202020;
+            color3 = String.Format("#{0:X6}", hex3);
+            var hex4 = 0xFFFFFF ^ basecolor;
+            color4 = String.Format("#{0:X6}", hex4);
+            var hex5 = hex4 + 0x202020;
+            color5 = String.Format("#{0:X6}", hex5);
+            var hex6 = hex5 + 0xF0F0F0;
+            color6 = String.Format("#{0:X6}", hex6);
+        }
+
+        private void crazy(int basecolor, ref string color2, ref string color3, ref string color4, ref string color5, ref string color6) {
+
+            var hex2 = basecolor + 0x202020;
+            color2 = String.Format("#{0:X6}", hex2);
+            var hex3 = 0x808080 ^ basecolor;
+            color3 = String.Format("#{0:X6}", hex3);
+            var hex4 = hex3 + 0x202020;
+            color4 = String.Format("#{0:X6}", hex4);
+            var hex5 = 0xF0F0F0 ^ basecolor;
+            color5 = String.Format("#{0:X6}", hex5);
+            var hex6 = hex5 + 0x202020;
+            color6 = String.Format("#{0:X6}", hex6);
+        }
+
+        private void smooth(int basecolor, ref string color2, ref string color3, ref string color4, ref string color5, ref string color6) {
+
+            var hex2 = basecolor + 0x101010;
+            color2 = String.Format("#{0:X6}", hex2);
+            var hex3 = hex2 + 0x101010;
+            color3 = String.Format("#{0:X6}", hex3);
+            var hex4 = hex3 + 0x101010;
+            color4 = String.Format("#{0:X6}", hex4);
+            var hex5 = hex4 + 0x101010;
+            color5 = String.Format("#{0:X6}", hex5);
+            var hex6 = hex5 + 0x101010;
+            color6 = String.Format("#{0:X6}", hex6);
         }
 
         private void toClipboard(object sender, PointerRoutedEventArgs e) {
